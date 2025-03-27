1e454b69f87596d51ef53700b603c82af0f9f83a
FrancoFabian76 <francancela2017@gmail.com>
2025-03-27 14:41:38 -0300

    Realice la actividad junto a otros ejemplos.

--- hola.php
+++ hola.php
@@ -1,6 +1,47 @@
-<?php echo "Hola Mundo"; // amen
-#dhaufa
-/* fjauifo
-gka`kgh */
+<?php echo "Hola Mundo"; 
+
+// Tipos de comentarios:
+
+// (//) Solo de 1 linea
+# (#) Solo de 1 linea
+/* (/*) Sirve para dejar un comentario multilinea, siempre debe terminar con (* /) */
+
+
+
+// Tipos de Datos en PHP:
+
+$nombre = "Manolo"; // Texto. Puede ser con comillas simples o dobles (String)
+
+$edad = 31; // Enteros (Integer)
+
+$altura = 1.72; // Decimales, usando el punto (.) en lugar de la coma (;). (Float)
+
+$mayorEdad = true; // Verdad o falso (Boolean)
+
+
+//Actividad:
+
+$nombreCompleto = "Juan Perez"; // La variable esta correcta.
+
+$Edad = 25; // La variable esta correcta.
+
+$estAtura = 1.75; // Lo haria mas simple al nombrar la variable, en vez de estAtura seria estatura solo para facilitar la escritura. Aun asi la variable esta correcta.
+
+$numero = "10"; // El numero no lleva comillas dobles o simples.
+
+Apellido = "Fernandez"; // Es innecesario crear una variable apellido teniendo una de NombreCompleto, tambien tiene el error de no llevar $ para dar inicio a la variable.
+
+pais_de_origen = "Uruguay" // No lleva $ al inicio para declarar variable.
+
+$salario = $2000; // El error que quiza esta mal es el uso de $ en el 2000 ya que si no fuera algo como $dosmil abriria otra variable. No es necesario utilziar $ si ya declaras el numero.
+
+
+
+
+
+
+
+
+
 
 ?> 
